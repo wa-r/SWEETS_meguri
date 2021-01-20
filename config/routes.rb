6a679986db242d1c8163e1484684d9e8269b00da
wa-r <nyuuuuun14@gmail.com>
2021-01-20 05:46:23 +0000

    ディレクトの階層変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  devise_for :members
   devise_for :admins
-  resources :members, only: [:show, :edit, :update]
-  get '/unsubscribe' => 'members#unsubscribe'
-  patch '/withdrawal' => 'members#withdrawal'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :members
+  scope module: :member do
+    resources :members, only: [:show, :edit, :update]
+    get '/unsubscribe' => 'members#unsubscribe'
+    patch '/withdrawal' => 'members#withdrawal'
+  end
 end
