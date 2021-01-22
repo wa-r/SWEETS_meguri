ca3cbe0d1a38a678aa3d0e89fe87e855b56def8a
wa-r <nyuuuuun14@gmail.com>
2021-01-22 01:53:54 +0000

    [追加] いいね機能非同期通信（途中）

--- app/controllers/member/tweet_likes_controller.rb
+++ app/controllers/member/tweet_likes_controller.rb
@@ -1,15 +1,17 @@
 class Member::TweetLikesController < ApplicationController
 
   def create
-    tweet_like = current_member.tweet_likes.new(tweet_id: params[:tweet_id])
+    @tweet = Tweet.find(params[:tweet_id])
+    tweet_like = current_member.tweet_likes.new(tweet_id: tweet.id)
     tweet_like.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    tweet_like = TweetLike.find_by(tweet_id: params[:tweet_id], member_id: current_member.id)
+    @tweet = Tweet.find(params[:tweet_id])
+    tweet_like = current_member.tweet_likes.find_by(tweet_id: tweet.id)
     tweet_like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
