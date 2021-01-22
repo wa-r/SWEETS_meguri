32a7d574b15db76bf5fc844a0c60d2104365d6d4
wa-r <nyuuuuun14@gmail.com>
2021-01-22 03:56:01 +0000

    [修正] いいね機能

--- app/controllers/member/tweet_likes_controller.rb
+++ app/controllers/member/tweet_likes_controller.rb
@@ -2,14 +2,14 @@ class Member::TweetLikesController < ApplicationController
 
   def create
     @tweet = Tweet.find(params[:tweet_id])
-    tweet_like = current_member.tweet_likes.new(tweet_id: tweet.id)
+    tweet_like = current_member.tweet_likes.new(tweet_id: @tweet.id)
     tweet_like.save
     # redirect_to request.referer
   end
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
-    tweet_like = current_member.tweet_likes.find_by(tweet_id: tweet.id)
+    tweet_like = current_member.tweet_likes.find_by(tweet_id: @tweet.id)
     tweet_like.destroy
     # redirect_to request.referer
   end
