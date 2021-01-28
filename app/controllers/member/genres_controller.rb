f99447d0fcfd8190780f687f3ea27e73e8a967d8
wa-r <nyuuuuun14@gmail.com>
2021-01-28 13:34:35 +0000

    [追加] ジャンルごとの検索リンク

--- app/controllers/member/genres_controller.rb
+++ app/controllers/member/genres_controller.rb
@@ -1,25 +1,27 @@
 class Member::GenresController < ApplicationController
   
   def cake
-    @genre_cakes = Shop.where(genre_id: 1)
+    @cakes_shops = Shop.where(genre_id: 1)
   end
   
   def macaron
+    @macaron_shops = Shop.where(genre_id: 2)
   end
   
   def chocolate
+    @chocolate_shops = Shop.where(genre_id: 3)
   end
   
   def parfait
+    @parfait_shops = Shop.where(genre_id: 4)
   end
   
   def cafe
+    @cafe_shops = Shop.where(genre_id: 5)
   end
   
   def shaved_ice
+    @shaved_ice_shops = Shop.where(genre_id: 6)
   end
-  
-  def other
-  end
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Genre.create!(
-  name: "ケーキ"
-  )
+Admin.create!(
+  [
+    {
+      name: "田中",
+      email: "tanaka@tanaka.com",
+      password: "123456"
+    },
+    {
+      name: "佐藤",
+      email: "satou@satou.com",
+      password: "123456"
+    }
+  ]
+)
+
+# Member.create!(
+#   [
+#     {
+#       name: "あゆみ",
+#       email: "ayumi@ayumi.com",
+#       password: "123456",
+#       prpfile_image: File.open('./app/assets/images/test.jpg'),
+#       introduction: "",
+#       is_deleted: "false"
+#     }
+#   ]
+# )
   
 Genre.create!(
-  name: "パフェ"
-  )
-
-Genre.create!(
-  name: "カフェ"
-  )
-  
-Genre.create!(
-  name: "かき氷"
-  )
-
-Genre.create!(
-  name: "その他"
-  )
+  [
+    {
+      name: "ケーキ"
+    },
+    {
+      name: "マカロン"
+    },
+    {
+      name: "チョコレート"
+    },
+    {
+      name: "パフェ"
+    },
+    {
+      name: "カフェ"
+    },
+    {
+      name: "かき氷"
+    }
+  ]
+)
