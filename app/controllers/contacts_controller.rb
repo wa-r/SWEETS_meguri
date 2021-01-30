19cd5920b1c5b8f798681c070aca2fe616385310
wa-r <nyuuuuun14@gmail.com>
2021-01-30 07:40:09 +0000

    [追加] お問い合わせ機能

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.contact_mail(@contact).deliver
+      redirect_to contacts_thanks_path
+    else
+      flash.now[:alert] = "入力内容をもう一度確認してください"
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :title, :message)
+  end
+
+end
