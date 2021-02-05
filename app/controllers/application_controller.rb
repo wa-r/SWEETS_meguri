29c949050a539675c847523452e6706000e95046
wa-r <nyuuuuun14@gmail.com>
2021-02-05 18:28:00 +0000

    [追加] ログイン・新規登録後の遷移先指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # 新規登録後
+  def after_sign_up_path_for(resource)
+    if member_signed_in?
+      member_path(current_member)
+    else
+      admins_shops_path
+    end
+  end
+
+  # ログイン後
+  def after_sign_in_path_for(resource)
+    if member_signed_in?
+      member_path(current_member)
+    else
+      admin_shops_path
+    end
+  end
+
+  # ログアウト後
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
