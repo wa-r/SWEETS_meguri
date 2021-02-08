f72ee5e05574abc509c9eaff5cea11b572803415
wa-r <nyuuuuun14@gmail.com>
2021-02-08 01:42:13 +0000

    [追加] ランキング機能(途中)

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,13 @@ class HomesController < ApplicationController
   def top
     @q = Shop.ransack(params[:q])
     @shops = @q.result(distinct: true)
+    
+    
+    # @aves =  []
+    # Shop.all.each do |shop|
+    #   @aves.push shop.reviews.average(:rate).to_f.round(1)
+    # end
   end
-
+  
 end
+
