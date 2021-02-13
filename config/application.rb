7eac8253b82731bce60368ecc845d8953a879ab9
wa-r <nyuuuuun14@gmail.com>
2021-02-13 17:27:02 +0000

    [修正] Bookmark画面の星のid/エラーメッセージ

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,10 @@ module SWEETSMeguri
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
